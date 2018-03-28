6102b61b30f72ece832de93109a49728c0838b13
thando <tzone85@gmail.com>
2018-03-28 19:13:43 +0200

    created roles migration and updated the create function in the users create users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,15 @@ class CreateUsersTable extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
+
+        Schema::table('users', function (Blueprint $table) {
+            %$table->string('first_name')->nullable();;
+            %$table->string('middle_name')->nullable();;
+            %$table->string('last_name')->nullable();;
+            %$table->string('city')->nullable();
+            $table->integer('role_id')->unsigned();
+
+        });
     }
 
     /**
