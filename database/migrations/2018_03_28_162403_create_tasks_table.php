3da746333e49b06e51440599ab1fd8e9381d1f3c
thando <tzone85@gmail.com>
2018-03-28 18:43:18 +0200

    created migration for tasks and added the linkage between the tasks and projects tables

--- database/migrations/2018_03_28_162403_create_tasks_table.php
+++ database/migrations/2018_03_28_162403_create_tasks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProjectsTable extends Migration
+class CreateTasksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,17 +13,17 @@ class CreateProjectsTable extends Migration
      */
     public function up()
     {
-        Schema::create('projects', function (Blueprint $table) {
+        Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->longText('description');
-            $table->integer('company_id')->unsigned();
+            $table->integer('project_id')->unsigned();
             $table->integer('user_id')->unsigned();
             $table->integer('days')->unsigned();
+            $table->integer('hours')->unsigned();
 
             $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('project_id')->references('id')->on('projects');
             $table->foreign('company_id')->references('id')->on('companies');
-
             $table->timestamps();
         });
     }
@@ -35,6 +35,6 @@ class CreateProjectsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('projects');
+        Schema::dropIfExists('tasks');
     }
 }
