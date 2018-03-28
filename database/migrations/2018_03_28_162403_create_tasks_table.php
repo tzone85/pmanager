0ab1f1558eca4871377e6903721f691572bd1ac2
thando <tzone85@gmail.com>
2018-03-28 19:03:56 +0200

    added a comments table, with the relations with all the other tables. As well as made all the columns that can be nullable, nullable

--- database/migrations/2018_03_28_162403_create_tasks_table.php
+++ database/migrations/2018_03_28_162403_create_tasks_table.php
@@ -18,8 +18,9 @@ class CreateTasksTable extends Migration
             $table->string('name');
             $table->integer('project_id')->unsigned();
             $table->integer('user_id')->unsigned();
-            $table->integer('days')->unsigned();
-            $table->integer('hours')->unsigned();
+            $table->integer('days')->unsigned()->nullable();
+            $table->integer('hours')->unsigned()->nullable();
+            $table->integer('company_id')->unsigned()->nullable();
 
             $table->foreign('user_id')->references('id')->on('users');
             $table->foreign('project_id')->references('id')->on('projects');
---
+++ database/migrations/2018_03_28_164350_create_comments_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->longText('body');
+            $table->string('url', 255)->nullable();                      //a string pointing towards the work that they have doen for the day
+            $table->integer('user_id')->unsigned();
+
+//          Porlymorphic relationships under Eloquent Relationships
+
+            $table->integer('commentable_id')->unsigned();
+            $table->string('commentable_type');                                 // this one will contain the model
+
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
