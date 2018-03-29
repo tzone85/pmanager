6b68e9856be76d72b2f85f615c504c822a19027d
thando <tzone85@gmail.com>
2018-03-29 03:22:35 +0200

    created companies controller. Then added all the other controllers in the route by making use of resource, i.e. resource controller

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,13 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('companies', 'CompaniesController');
+
+Route::resource('projects', 'ProjectsController');
+
+Route::resource('roles', 'RolesController');
+
+Route::resource('tasks', 'TasksController');
+
+Route::resource('users', 'UsersController');
