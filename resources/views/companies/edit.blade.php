e8bfab12d0faf7e29afb4e3f9670dc9e6c1db92c
thando <tzone85@gmail.com>
2018-04-02 12:15:08 +0200

    finished making the form for editing. Retrieved the list of companies on the right side bar

--- resources/views/companies/edit.blade.php
+++ resources/views/companies/edit.blade.php
@@ -6,8 +6,45 @@
 
         <!-- Example row of columns -->
 
+
+        {{--Inserting the form--}}
         <div class="row col-md-12 col-lg-12 col-sm-12" style="background: white; margin: 10px;">
 
+            <form action="{{ route('companies.update', [$company->id]) }}" method="post">
+                {{ csrf_token() }}
+
+                <input type="hidden" name="_method" value="put">
+
+                <div class="form-group">
+                    <label for="company-name">Name<span class="required">*</span></label>
+                    <input type="text"
+                            placeholder="Enter Name"
+                            id="company-name"
+                            required
+                            name="description"
+                            spellcheck="false"
+                            class="form-control"
+                            value="{{ $company->name }}"
+                    />
+
+                </div>
+
+                <div class="form-group">
+                    <label for="company-content">Description</label>
+                    <textArea placeholder="Enter description"
+                                style="resize: vertical"
+                                id="company-content"
+                                name="description"
+                                rows="5"
+                                spellcheck="false"
+                                class="form-control autosize-target text-left">{{ $company->description }}
+                    </textArea>
+                </div>
+
+                <div class="form-group">
+                    <input type="submit" class="btn btn-primary" value="Submit">
+                </div>
+            </form>
 
         </div>
     </div>
@@ -21,9 +58,11 @@
         <div class="sidebar-module">
             <h4>Actions</h4>
             <ol class="list-unstyled">
-                <li><a href="/companies/{{$company->id}}/edit">Edit</a></li>
-                <li><a href="#">Delete</a></li>
-                <li><a href="#">Add New Members</a></li>
+                <li><a href="/companies/{{$company->id}}">View copanies</a></li>
+                <li><a href="/companies">All copanies</a></li>
+                
+
+
             </ol>
         </div>
 
